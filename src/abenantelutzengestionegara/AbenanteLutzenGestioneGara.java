71ceef0ccf200f7774b9129d20c1ad0b1e18aa05
lutzenjacopo <lutzen.jacopo@franchettisalviani.net>
2026-03-03 11:46:29 +0100

    Initial commit

---
+++ src/abenantelutzengestionegara/AbenanteLutzenGestioneGara.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package abenantelutzengestionegara;
+
+/**
+ *
+ * @author lutzen.jacopo
+ */
+public class AbenanteLutzenGestioneGara {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
