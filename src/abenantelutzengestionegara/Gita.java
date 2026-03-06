a360eb5845c93ac8f89716611ca8d11493ccb75f
Lu0804 <abenante.lucia@franchettisalviani.net>
2026-03-06 12:30:52 +0100

    Studenti e gita

--- src/abenantelutzengestionegara/Gita.java
+++ src/abenantelutzengestionegara/Gita.java
@@ -10,4 +10,73 @@ package abenantelutzengestionegara;
  */
 public class Gita {
     
+    private int id;
+    private String localita;
+    private int durata;
+/**
+ * costruttore 
+ * @param id id della gita
+ * @param localita località della gita 
+ * @param durata durata in giorni della gita 
+ */
+    public Gita(int id, String localita, int durata) {
+        this.id = id;
+        this.localita = localita;
+        this.durata = durata;
+    }
+
+    
+    // Get & Set
+    public int getId() {
+        return id;
+    }
+
+    public String getLocalita() {
+        return localita;
+    }
+
+    public int getDurata() {
+        return durata;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLocalita(String localita) {
+        this.localita = localita;
+    }
+
+    public void setDurata(int durata) {
+        this.durata = durata;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Gita other = (Gita) obj;
+        return this.id == other.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Gita con id=" + id + " a " + localita + " di durata " + durata + " giorni";
+    }
+    
+    
 }
--- src/abenantelutzengestionegara/Studente.java
+++ src/abenantelutzengestionegara/Studente.java
@@ -9,5 +9,88 @@ package abenantelutzengestionegara;
  * @author lutzen.jacopo
  */
 public class Studente {
+
+    private String nome;
+    private String cognome;
+    private int matricola;
+    private int anno;
     
+    
+/**
+ * costruttore 
+ * @param nome nome dello studente 
+ * @param cognome cognome dello studente 
+ * @param matricola matricola dello studente 
+ * @param anno anno scolastico dello studente 
+ */
+    public Studente(String nome, String cognome, int matricola, int anno) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.matricola = matricola;
+        this.anno = anno;
+
+    }
+
+    
+    
+ // Get & Set
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public void setMatricola(int matricola) {
+        this.matricola = matricola;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public int getMatricola() {
+        return matricola;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + this.matricola;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Studente other = (Studente) obj;
+        return this.matricola == other.matricola;
+    }
+
+    @Override
+    public String toString() {
+        return " " + nome + " " + cognome + " con matricola " + matricola;
+    }
+
 }
