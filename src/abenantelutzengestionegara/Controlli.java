2e1ac409f9f9472405f2e7f29c8d131487139f76
lutzenjacopo <lutzen.jacopo@franchettisalviani.net>
2026-03-06 12:46:23 +0100

    blblbblbblbllbll

---
+++ src/abenantelutzengestionegara/Controlli.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abenantelutzengestionegara;
+
+/**
+ *
+ * @author lutzen.jacopo
+ */
+public class Controlli {
+    
+    
+    public String aggiustaLunghezzaStringa(String s) {
+        String aggiustata=s;
+        if (s.length() < 20) {
+            for (int i = 0; i < (20 - s.length()); i++) {
+                aggiustata += "*";
+            }
+            return aggiustata;
+        } else if (s.length() > 20) {
+            aggiustata = s.substring(0, 19);
+            return aggiustata;
+        }
+        return s;
+    }
+    
+    /**
+     * controlla se una variabile String è un int
+     *
+     * @param n parametro da controllare
+     * @return
+     */
+    public boolean controlloInt(String n) {
+        try {
+            //provo a convertire il parametro in un int
+            int numero = Integer.parseInt(n);
+        } catch (NumberFormatException e) {
+            // n è una Stringa
+            System.out.println("Errore: Il dato inserito non è un numero valido!");
+            return false;
+        }
+        // n è un intero
+        return true;
+    }
+
+    /**
+     * controlla se una variabile String è una String
+     *
+     * @param n parametro da controllare
+     * @return
+     */
+    public boolean controlloString(String n) {
+        //provo a convertire il parametro in un int
+        try {
+            int numero = Integer.parseInt(n);
+        } catch (NumberFormatException e) {
+            // n è una Stringa
+            return true;
+        }
+        // n è un intero
+        System.out.println("Errore: Il dato inserito è un numero !");
+        return false;
+    }
+
+    /**
+     * controlla se una variabile String è nulla
+     *
+     * @param n parametro da controllare
+     * @return
+     */
+    public boolean controlloNull(String n) {
+        if (!n.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
--- src/abenantelutzengestionegara/GestioneFileStudenti.java
+++ src/abenantelutzengestionegara/GestioneFileStudenti.java
@@ -4,10 +4,33 @@
  */
 package abenantelutzengestionegara;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 /**
  *
  * @author lutzen.jacopo
  */
 public class GestioneFileStudenti {
+    private int matricola=4,nome=40,cognome=40,anno=4;
+    private static final int DIM_RECORD =88;
+    public boolean creaFileStudente(){
+        try {
+            RandomAccessFile file = new RandomAccessFile("elenco.dat", "r");
+            //calcolo la dimensione del file per capire quanti record ci sono. 
+             int nRecord = (int) (file.length() / DIM_RECORD);
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("File non trovato");
+            return false;
+        } catch (IOException e) {
+            System.out.println("Problema in lettura-scrittura file");
+            return false;
+        }
+        return true;
+    }
+    
     
 }
+
